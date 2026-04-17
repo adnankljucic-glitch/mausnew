0035348ffce46bbd557230d7c10e773de013a941
adnankljucic-glitch <adnankljucic@gmail.com>
2026-04-17 16:40:15 +0200

    Add ExpertisePillars to Expertise page

--- src/pages/ExpertisePage.tsx
+++ src/pages/ExpertisePage.tsx
@@ -1,20 +1,18 @@
 import ExpertiseHero from '../components/ExpertiseHero';
 import HeroServicesWrapper from '../components/HeroServicesWrapper';
+import ExpertisePillars from '../components/ExpertisePillars';
 import OutcomesSection from '../components/OutcomesSection';
 import ProcessAccordion from '../components/ProcessAccordion';
 import ReadySection from '../components/ReadySection';
-
 export default function ExpertisePage() {
   return (
     <>
       <HeroServicesWrapper variant="expertise">
         <ExpertiseHero />
       </HeroServicesWrapper>
-
+      <ExpertisePillars />
       <OutcomesSection />
-
       <ProcessAccordion />
-
       <ReadySection />
     </>
   );
