96e709b08e06a84cc6b2050054b4bb0dfee031b9
adnankljucic-glitch <adnankljucic@gmail.com>
2026-04-19 22:03:21 +0200

    Updated index.css

--- src/pages/ExpertisePage.tsx
+++ src/pages/ExpertisePage.tsx
@@ -1,7 +1,6 @@
 import ExpertiseHero from '../components/ExpertiseHero';
 import HeroServicesWrapper from '../components/HeroServicesWrapper';
 import ExpertisePillarsHero from '../components/ExpertisePillarsHero';
-import ExpertisePillarsTabs from '../components/ExpertisePillarsTabs';
 import OutcomesSection from '../components/OutcomesSection';
 import ProcessAccordion from '../components/ProcessAccordion';
 import ReadySection from '../components/ReadySection';
@@ -12,7 +11,6 @@ export default function ExpertisePage() {
         <ExpertiseHero />
       </HeroServicesWrapper>
       <ExpertisePillarsHero />
-      <ExpertisePillarsTabs />
       <OutcomesSection />
       <ProcessAccordion />
       <ReadySection />
